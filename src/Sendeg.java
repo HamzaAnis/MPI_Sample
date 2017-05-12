a6cd31f076e5f28dc048d4ddcadc0df557440013
HamzaAnis <hamzaanis9514@gmail.com>
2017-05-12 17:17:02 +0500

    Broad Castting the message

--- src/Sendeg.java
+++ src/Sendeg.java
@@ -7,12 +7,14 @@ public class Sendeg {
 		int size = MPI.COMM_WORLD.Size();
 		if (rank == 0) {
 			char[] seql = "Hello Slave".toCharArray();
-			MPI.COMM_WORLD.Send(seql, 0, seql.length, MPI.CHAR, 3, 99);
+			MPI.COMM_WORLD.Bcast(seql, 0, seql.length, MPI.CHAR, 0);
+//			MPI.COMM_WORLD.Send(seql, 0, seql.length, MPI.CHAR, 3, 99);
 		} else {
 			char[] seql = new char[100];
-			MPI.COMM_WORLD.Recv(seql, 0, seql.length, MPI.CHAR, 0, 99);
+			MPI.COMM_WORLD.Bcast(seql, 0, seql.length, MPI.CHAR, 0);
+//			MPI.COMM_WORLD.Recv(seql, 0, seql.length, MPI.CHAR, 0, 99);
 			System.out.println("I am salve "+rank+" of size "+(size-1)+" ");
-//			System.out.println("Received from master "+new String(seql));
+			System.out.println("Received from master "+new String(seql));
 		}
 		MPI.Finalize();
 	}
