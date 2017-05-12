dbb1cbc0a774e9b9a97eddb010417dffb61ca969
HamzaAnis <hamzaanis9514@gmail.com>
2017-05-12 17:14:40 +0500

    Mpi Receive and send.

--- src/Sendeg.java
+++ src/Sendeg.java
@@ -1,14 +1,19 @@
 import mpi.*;
-public class Sendeg{
-	public static void main(String args[])throws Exception{
+
+public class Sendeg {
+	public static void main(String args[]) throws Exception {
 		MPI.Init(args);
-		int rank=MPI.COMM_WORLD.Rank();
-		int me =MPI.COMM_WORLD.Rank();
-		int size=MPI.COMM_WORLD.Size();
-		if(rank==0){
-			char[] seql="Hello Slave".toCharArray();
+		int rank = MPI.COMM_WORLD.Rank();
+		int size = MPI.COMM_WORLD.Size();
+		if (rank == 0) {
+			char[] seql = "Hello Slave".toCharArray();
+			MPI.COMM_WORLD.Send(seql, 0, seql.length, MPI.CHAR, 3, 99);
+		} else {
+			char[] seql = new char[100];
+			MPI.COMM_WORLD.Recv(seql, 0, seql.length, MPI.CHAR, 0, 99);
+			System.out.println("I am salve "+rank+" of size "+(size-1)+" ");
+//			System.out.println("Received from master "+new String(seql));
 		}
-//		System.out.println("Hello from <"+me+"> of total <"+size+">");
 		MPI.Finalize();
 	}
 }
